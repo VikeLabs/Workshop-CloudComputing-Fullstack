fe6bac9fc91a9400594ef0552efa68c0391272af
Brennan McMicking <brennanmcmicking@gmail.com>
2024-03-05 10:04:49 -0800

    change: refactor out data and add better error handling

---
+++ rest-backend/backend.py
@@ -0,0 +1,34 @@
+import json
+
+from flask import Flask
+from flask_cors import CORS, cross_origin
+
+from mockdata import data as mock_data
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+def get_data(id: int): 
+    return mock_data[id]
+
+
+@app.route("/")
+def slash():
+    return "hello, world"
+
+
+@app.route("/product/<id>")
+@cross_origin()
+def product(id):
+    try:
+        data = get_data(id)
+        return json.dumps({
+            "nam": data["name"],
+            "img": data["imageUrl"],
+            "pri": data["price"],
+        }), 200
+    except:
+        print(f"An error occurred while trying to get product information, id: {id}")
+        return json.dumps({}), 400
---
+++ rest-backend/mockdata.py
@@ -0,0 +1,29 @@
+
+
+
+data = {
+    "10000": {
+        "name": "Mark Laidlaw",
+        "imageUrl": "https://www.uvic.ca/science/physics/assets/images/profile/laidlaw.jpg",
+        "price": "420.69",
+        "sellerName": "The UVic Archives"
+    },
+    "20000": {
+        "name": "uh oh",
+        "imageUrl": "https://i1.sndcdn.com/artworks-000634543696-hl5u8u-t500x500.jpg",
+        "price": "5.00",
+        "sellerName": "McMorran's Symposium"
+    },
+    "30000": {
+        "name": "Site C Dam",
+        "imageUrl": "https://energeticcity.ca/wp-content/uploads/2023/01/site-c-5.jpg",
+        "price": "16,000,000,000.00",
+        "sellerName": "Feltham House"
+    },
+    "40000": {
+        "name": "maxwell :3",
+        "imageUrl": "https://i.scdn.co/image/ab67616d0000b27327047720beaa8d2b4c236380",
+        "price": "100.00",
+        "sellerName": "McMorran's Symposium"
+    }
+}
